b7dfbcbae66307bbeb45c034d92e3d1dd647e087
wlk0202 <54826094+wlk0202@users.noreply.github.com>
2019-12-17 11:19:01 +0900

    Add files via upload

---
+++ Ftn4.cpp
@@ -0,0 +1,14 @@
+﻿// Ftn4.cpp : 정적 라이브러리를 위한 함수를 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: 라이브러리 함수의 예제입니다.
+void fnFtn4()
+{
+}
+int Add(int a, int b) {
+
+		return a + b;
+}
