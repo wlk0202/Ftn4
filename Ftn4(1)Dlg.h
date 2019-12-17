956f2f4f4c1983c5adf6ae82b1cc7d2eda5d47fd
wlk0202 <54826094+wlk0202@users.noreply.github.com>
2019-12-17 11:18:12 +0900

    Add files via upload

---
+++ Ftn4(1)Dlg.h
@@ -0,0 +1,36 @@
+﻿
+// Ftn4(1)Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CFtn41Dlg 대화 상자
+class CFtn41Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CFtn41Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FTN41_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
